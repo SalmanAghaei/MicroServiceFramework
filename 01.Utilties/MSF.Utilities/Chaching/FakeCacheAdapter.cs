3ff4c10c78214f96fd2c9fa7dc91e3ffebfa61e0
Mostafa Aghaei <Mostafa_Aghaei@hotmail.com>
2021-12-10 21:36:32 +0330

    uy

--- 01.Utilties/MSF.Utilities/Chaching/FakeCacheAdapter.cs
+++ 01.Utilties/MSF.Utilities/Chaching/FakeCacheAdapter.cs
@@ -10,6 +10,10 @@ namespace MSF.Utilities.Chaching
         public void Add<TInput>(string key, TInput obj, DateTime? AbsoluteExpiration, TimeSpan? SlidingExpiration)
         { }
 
+
+
+
+
         public TOutput Get<TOutput>(string key) => default;
 
         public void RemoveCache(string key)
